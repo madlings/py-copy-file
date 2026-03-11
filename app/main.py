59be17c8f74cca03efcfcc22c3018945b98efd58
madlings <nagalingam.s@gmail.com>
2026-03-11 21:42:32 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -9,6 +9,5 @@ def copy_file(command: str) -> None:
     # Do nothing if the source and destination names are the same
     if source_name == destination_name:
         return
-    with (open(source_name, "r") as file_in, 
-        open(destination_name, "w") as file_out):
+    with open(source_name, "r") as file_in, open(destination_name, "w") as file_out:
         file_out.write(file_in.read())
